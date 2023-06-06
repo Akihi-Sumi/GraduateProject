d03ead19343871d9d16199bdc391f86c14bba6ae
Motoki Kutsuna <motoki20020813@gmail.com>
2023-06-06 10:59:39 +0900

    qrコードを追加

--- lib/ui/settings/mypage.dart
+++ lib/ui/settings/mypage.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
+
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  TextEditingController textEditingController1 = TextEditingController();
+  TextEditingController textEditingController2 = TextEditingController();
+  String name = ''; // 名前を保持するプロパティ
+  String email = ''; // メールアドレスを保持するプロパティ
+
+  @override
+  void dispose() {
+    textEditingController1.dispose();
+    textEditingController2.dispose();
+    super.dispose();
+  }
+
+  void updateInputValue1(String value) {
+    setState(() {
+      name = value;
+    });
+  }
+
+  void updateInputValue2(String value) {
+    setState(() {
+      email = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +45,77 @@ class MyPage extends StatelessWidget {
           ),
         ),
         backgroundColor: Colors.orange.shade700,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textEditingController1,
+              onChanged: (value) {
+                updateInputValue1(value);
+              },
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: '名前を入力',
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange.shade700),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '名前: $name',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: textEditingController2,
+              onChanged: (value) {
+                updateInputValue2(value);
+              },
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'メールアドレスを入力',
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange.shade700),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'メールアドレス: $email',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyPage(),
+                  ),
+                );
+              },
+              child: Text("保存"),
+            ),
+          ],
+        ),
       ),
     );
   }

aaf6cdacc6a2360bcdd281f5fe2c30a21e7214c2
Akihi-Sumi <akiakisander@gmail.com>
2023-06-07 15:08:55 +0900

    0607 - Akihi

--- lib/fan_floating_menu/fan_floating_menu.dart
+++
@@ -1,4 +0,0 @@
-library simple_speed_dial;
-
-export 'src/simple_speed_dial.dart';
-export 'src/simple_speed_dial_child.dart';
--- lib/ui/settings/edit_message.dart
+++ lib/ui/settings/edit_message.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:graduate_app/fan_floating_menu/src/simple_speed_dial.dart';
-import 'package:graduate_app/fan_floating_menu/src/simple_speed_dial_child.dart';
+import 'package:graduate_app/fan/fan_bubble.dart';
 
 class EditMessagePage extends StatefulWidget {
   const EditMessagePage({Key? key}) : super(key: key);
@@ -9,68 +8,96 @@ class EditMessagePage extends StatefulWidget {
   _EditMessagePageState createState() => _EditMessagePageState();
 }
 
-class _EditMessagePageState extends State<EditMessagePage> {
-  String _text = "";
+class _EditMessagePageState extends State<EditMessagePage>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 260),
+    );
+
+    final curvedAnimation =
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+    _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: const Text(
-            "メッセージ設定",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            title: const Text(
+              "メッセージ設定",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
             ),
+            backgroundColor: Colors.orange.shade700,
           ),
-          backgroundColor: Colors.orange.shade700,
-        ),
-        body: Center(child: Text(_text)),
-        floatingActionButton: SpeedDial(
-          speedDialChildren: <SpeedDialChild>[
-            SpeedDialChild(
-              child: const Icon(Icons.directions_run),
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.red,
-              label: 'Let\'s go for a run!',
-              onPressed: () {
-                setState(() {
-                  _text = 'You pressed "Let\'s go for a run!"';
-                });
-              },
-            ),
-            SpeedDialChild(
-              child: const Icon(Icons.directions_walk),
-              foregroundColor: Colors.black,
-              backgroundColor: Colors.yellow,
-              label: 'Let\'s go for a walk!',
-              onPressed: () {
-                setState(() {
-                  _text = 'You pressed "Let\'s go for a walk!"';
-                });
-              },
-            ),
-            SpeedDialChild(
-              child: const Icon(Icons.directions_bike),
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.green,
-              label: 'Let\'s go cycling!',
-              onPressed: () {
-                setState(() {
-                  _text = 'You pressed "Let\'s go cycling!"';
-                });
-              },
-            ),
-          ],
-          closedForegroundColor: Colors.black,
-          openForegroundColor: Colors.white,
-          closedBackgroundColor: Colors.white,
-          openBackgroundColor: Colors.black,
-          child: const Icon(Icons.add),
-        ),
-      ),
+          //body: Center(child: Text(_text)),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          floatingActionButton: FloatingActionBubble(
+            items: <Bubble>[
+              Bubble(
+                title: "Settings",
+                iconColor: Colors.white,
+                bubbleColor: Colors.blue,
+                icon: Icons.settings,
+                titleStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                onPress: () {
+                  _animationController.reverse();
+                },
+              ),
+              // Floating action menu item
+              Bubble(
+                title: "Profile",
+                iconColor: Colors.white,
+                bubbleColor: Colors.blue,
+                icon: Icons.people,
+                titleStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                onPress: () {
+                  _animationController.reverse();
+                },
+              ),
+              //Floating action menu item
+              Bubble(
+                title: "Home",
+                iconColor: Colors.white,
+                bubbleColor: Colors.blue,
+                icon: Icons.home,
+                titleStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                onPress: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const EditMessagePage()));
+                  _animationController.reverse();
+                },
+              ),
+            ],
+            animation: _animation,
+
+            // On pressed change animation state
+            onPress: () => _animationController.isCompleted
+                ? _animationController.reverse()
+                : _animationController.forward(),
+
+            // Floating Action button Icon color
+            iconColor: Colors.blue,
+
+            // Floating Action button Icon
+            iconData: Icons.ac_unit,
+            backGroundColor: Colors.white,
+          )),
     );
   }
 }
